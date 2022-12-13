081a84ba25db1dcdddf23ff0e9e115629137a4b9
xplus2 <33136922+xplus2@users.noreply.github.com>
2022-12-13 19:47:27 +0100

    Update const.py
    removed ADB

--- custom_components/ambient_extractor/const.py
+++ custom_components/ambient_extractor/const.py
@@ -1,7 +1,6 @@
 """Constants for the ambient_extractor component."""
 ATTR_PATH = "ambient_extract_path"
 ATTR_URL = "ambient_extract_url"
-ATTR_ADB = "ambient_extract_adb"
 
 ATTR_BRIGHTNESS_AUTO = "brightness_auto"
 ATTR_BRIGHTNESS_MODE = "brightness_mode"
